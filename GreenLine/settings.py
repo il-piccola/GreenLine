9c4caf3b19a7dfcad8fe9072a04d03cd915181de
Seiko Tachibana <il.piccola.estate@gmail.com>
2023-02-14 18:01:51 +0900

    20230214_01

--- GreenLine/settings.py
+++ GreenLine/settings.py
@@ -82,14 +82,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-if not DEBUG = {
+if not DEBUG :
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
