92e4d0fa71a3d54e2550b04bd8a0faf21d057cf6
Seiko Tachibana <il.piccola.estate@gmail.com>
2023-05-31 16:48:47 +0900

    20230531_03

--- GreenLine/migrations/0001_re_initial.py
+++ GreenLine/migrations/0001_re_initial.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a32db4f15f83e3bec159e35750f4e6ace602fddd0b85d693acb221360c57e8b2
-size 5915
+oid sha256:c4b360ef550f6c3e0c82283669f625e65e685c17f3d89fc21e266e01ebd720c8
+size 5916
